a72292930ccb49b3fc0a00be9fbfe8f1dfdb5f29
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-11-12 19:44:45 -0500

    added Shape enum

---
+++ Sources/TetrisEngine/Shape.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+enum Shape: Int8 {
+    case I = 1
+    case O = 2    
+    case T = 3
+    case S = 4
+    case Z = 5
+    case J = 6
+    case L =  7
+    case rage = 8
+
+    case empty = 127
+    case current = 126   
+}
