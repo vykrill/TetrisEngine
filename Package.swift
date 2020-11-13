74e652a9ba7e9184505a074607716401740cc7cb
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-11-12 19:39:31 -0500

    added CTetrisEngine as a dependency

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TetrisEngine",
-            dependencies: []),
+            dependencies: ["CTetrisEngine"]),
         .testTarget(
             name: "TetrisEngineTests",
             dependencies: ["TetrisEngine"]),
