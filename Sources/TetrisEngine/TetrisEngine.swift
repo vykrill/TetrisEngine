40bc342e3982966c6feaaab0f0308302ad0b1948
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-11-12 19:40:11 -0500

    created the TetrisGameHandler class

--- Sources/TetrisEngine/TetrisEngine.swift
+++ Sources/TetrisEngine/TetrisEngine.swift
@@ -1,1 +1,30 @@
 
+import Foundation
+import CTetrisEngine
+
+public class TetrisGameHandler {
+    private var cHandler: OpaquePointer
+
+    public init(configuration: TetrisGameConfiguration) {
+        self.cHandler = CTetrisCreateGameHandler(configuration)
+    }
+
+    public var width:  UInt32 { CTetrisGetMatrixWidth(cHandler)}
+    public var height: UInt32 { CTetrisGetMatrixHeight(cHandler)}
+
+    public func update(with timeDelta: Double) -> TetrisUpdateState {
+        return CTetrisUpdateGameHandler(cHandler, timeDelta)
+    }
+
+    public func perform(action: CTetrisActions) {
+        CTetrisPerformAction(cHandler, action)
+    }
+
+    public func unpause() {
+        CTetrisUnpauseGame(cHandler)
+    }
+
+    public func printStatus() {
+        CTetrisPrintGameStatus(cHandler)
+    }
+}
