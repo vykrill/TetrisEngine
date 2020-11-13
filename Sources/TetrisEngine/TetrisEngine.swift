453be658d2933f6d996277e8901a14fca5159144
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-11-12 20:09:51 -0500

    added methods to TetrisGameHandler

--- Sources/TetrisEngine/TetrisEngine.swift
+++ Sources/TetrisEngine/TetrisEngine.swift
@@ -12,8 +12,16 @@ public class TetrisGameHandler {
     public var width:  UInt32 { CTetrisGetMatrixWidth(cHandler)}
     public var height: UInt32 { CTetrisGetMatrixHeight(cHandler)}
 
+    public var currentShape: Shape {
+        return Shape(rawValue: CTetrisGetCurrentShapeIdentifier(cHandler))!
+    }
+
     public func update(with timeDelta: Double) -> TetrisUpdateState {
         return CTetrisUpdateGameHandler(cHandler, timeDelta)
+    }
+
+    public func getCellStatusAt(x: Int32, y: Int32) -> Shape {
+        Shape(rawValue: CTetrisGetCellStateAt(cHandler, x, y))!
     }
 
     public func perform(action: CTetrisActions) {
