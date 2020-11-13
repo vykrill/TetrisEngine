270f758c62a79a5c7213a7034c40419b2d04dbe3
Jérémy Goyette <jeremy.goyette00@gmail.com>
2020-11-12 22:02:47 -0500

    created a bunch of test

--- Tests/TetrisEngineTests/TetrisEngineTests.swift
+++ Tests/TetrisEngineTests/TetrisEngineTests.swift
@@ -2,14 +2,33 @@ import XCTest
 @testable import TetrisEngine
 
 final class TetrisEngineTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(TetrisEngine().text, "Hello, World!")
+    // If the compilation is successful, you should 
+    //  see "Tetris Engine Test" in the console and 
+    //  a big array of numbers.
+    func testCompilation() {
+        print("Tetris Engine Test")
+        let handler = TetrisGameHandler()
+        handler.printStatus()
+
+        let action = Action.hold
+        handler.perform(action: action)
+    }
+
+    // Tests custom parameters
+    func testCustomGrid() {
+        var config = TetrisGameHandler.defaultConfig
+        config.matrixHeight = 400
+        config.matrixWidth = 25
+        config.useRagePiece = true
+
+        let handler = TetrisGameHandler(configuration: config)
+
+        XCTAssertEqual(handler.width, 25)
+        XCTAssertEqual(handler.height, 400)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCompilation", testCompilation),
+        ("testConfig", testCustomGrid)
     ]
 }
